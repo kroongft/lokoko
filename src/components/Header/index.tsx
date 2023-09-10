8847b93a54a1a4dc2adf3831c7703ba2b12995fc
kroongft <krooooong@naver.com>
2023-09-10 22:53:54 +0900

    add: Header에 검색 ui 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { styled } from "styled-components";
+import { IconButton, InputBase, Paper } from "@mui/material";
+import { Search } from "@mui/icons-material";
 
 export const Header = () => {
-  return <HeaderWrapper>LOKOKO</HeaderWrapper>;
+  const [keyword, setKeyword] = useState<string>("");
+
+  return (
+    <HeaderWrapper>
+      <HeaderTitle>LOKOKO</HeaderTitle>
+      <Paper
+        component="form"
+        sx={{
+          p: "2px 4px",
+          display: "flex",
+          alignItems: "center",
+          width: 400,
+          background: "rgb(233, 233, 233);",
+          borderRadius: "12px",
+        }}
+      >
+        <InputBase
+          sx={{ ml: 1, flex: 1, fontSize: "14px" }}
+          placeholder="캐릭터 검색"
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+        <IconButton type="button" sx={{ fontSize: "1rem" }} aria-label="search">
+          <Search />
+        </IconButton>
+      </Paper>
+    </HeaderWrapper>
+  );
 };
 
 const HeaderWrapper = styled.div`
   padding: 24px;
-  display: flex;
+  display: grid;
   align-items: center;
-  justify-content: center;
+  grid-template-columns: auto 1fr;
+  gap: 100px;
+`;
+
+const HeaderTitle = styled.div`
   font-family: "EF_jejudoldam";
   font-size: 30px;
 `;
