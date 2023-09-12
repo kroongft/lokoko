b081215b8d28d989006bc099623f94b4cbd06dac
kroongft <krooooong@naver.com>
2023-09-12 23:16:44 +0900

    add: 엔터키, 검색 버튼 클릭시 검색 api 호출

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,31 @@
 import React, { useState } from "react";
 import { styled } from "styled-components";
+import { getArmories } from "../../api/armories";
 import { IconButton, InputBase, Paper } from "@mui/material";
 import { Search } from "@mui/icons-material";
 
 export const Header = () => {
   const [keyword, setKeyword] = useState<string>("");
 
+  const getCharacterProfiles = async () => {
+    await getArmories(keyword, "profiles");
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const trimKeyword = e.target.value.trim();
+    setKeyword(trimKeyword);
+  };
+
+  const handleOnKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      getCharacterProfiles();
+    }
+  };
+
   return (
     <HeaderWrapper>
       <HeaderTitle>LOKOKO</HeaderTitle>
       <Paper
-        component="form"
         sx={{
           p: "2px 4px",
           display: "flex",
@@ -24,9 +39,15 @@ export const Header = () => {
           sx={{ ml: 1, flex: 1, fontSize: "14px" }}
           placeholder="캐릭터 검색"
           value={keyword}
-          onChange={(e) => setKeyword(e.target.value)}
+          onChange={handleChange}
+          onKeyDown={handleOnKeyDown}
         />
-        <IconButton type="button" sx={{ fontSize: "1rem" }} aria-label="search">
+        <IconButton
+          type="button"
+          sx={{ fontSize: "1rem" }}
+          aria-label="search"
+          onClick={() => getCharacterProfiles()}
+        >
           <Search />
         </IconButton>
       </Paper>
@@ -45,4 +66,17 @@ const HeaderWrapper = styled.div`
 const HeaderTitle = styled.div`
   font-family: "EF_jejudoldam";
   font-size: 30px;
+`;
+
+const SearchWrapper = styled.div`
+  width: 300px;
+  padding: 8px;
+  background-color: rgb(233, 233, 233);
+  border-radius: 8px;
+  display: flex;
+`;
+const SearchInput = styled.input`
+  border: 0;
+  outline: none;
+  background-color: rgb(233, 233, 233);
 `;
